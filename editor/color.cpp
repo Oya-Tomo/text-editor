4b239c90e9f13038e69693c4175ebc985aab5078
Oya-Tomo <oyatomo.dev@gmail.com>
2022-06-14 18:44:01 +0900

    added function [ normal mode highlighting ] -beta

--- editor/color.cpp
+++ editor/color.cpp
@@ -9,9 +9,6 @@ static const regex htmlRegex2 = regex("([^ =\"]*)( *)(=)( *)(\"[\\S ]*?\")( *)")
 string coloringText(string text, int mode, int line, int x1, int y1, int x2, int y2)
 {
 	string target = text;
-	if (mode == NORMAL_MODE) {
-		return target;
-	}
 	if (mode == HTML_MODE) {
 		if (y1 == y2 && y1 == line) {
 			if (x1 < x2) {
@@ -45,6 +42,38 @@ string coloringText(string text, int mode, int line, int x1, int y1, int x2, int
 		}
 		else {
 			target = coloringHTML(target, 0, 0);
+		}
+		return target;
+	}
+	else {
+		if (y1 == y2 && y1 == line) {
+			if (x1 < x2) {
+				target = target.insert(x1, "\x1b[7m");
+				target = target.insert(x2 + 4, "\x1b[0m");
+			}
+			else if (x1 > x2) {
+				target = target.insert(x2, "\x1b[7m");
+				target = target.insert(x1 + 4, "\x1b[0m");
+			}
+		}
+		else if (y1 == line) {
+			if (y1 < y2) {
+				target = target.insert(x1, "\x1b[7m") + "\x1b[0m";
+			}
+			else {
+				target = "\x1b[7m" + target.insert(x1, "\x1b[0m");
+			}
+		}
+		else if (y2 == line) {
+			if (y1 < y2) {
+				target = "\x1b[7m" + target.insert(x2, "\x1b[0m");
+			}
+			else {
+				target = target.insert(x2, "\x1b[7m") + "\x1b[0m";
+			}
+		}
+		else if ((y1 < line && line < y2) || (y1 > line && line > y2)) {
+			target = "\x1b[7m" + target + "\x1b[0m";
 		}
 		return target;
 	}
