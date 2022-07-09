4a07279449d762f05648ec0e1d1d45d348bfa37a
Oya-Tomo <oyatomo.dev@gmail.com>
2022-07-09 16:09:53 +0900

    fixed function [ show a message box in the foreground ] -stable

--- editor/editor.cpp
+++ editor/editor.cpp
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
 	while (true)
 	{
 		if (isCloseMode) {
-			int status = MessageBox(NULL, TEXT("保存して終了しますか？"), TEXT("Text Editor"), MB_YESNOCANCEL | MB_ICONQUESTION);
+			int status = MessageBox(NULL, TEXT("保存して終了しますか？"), TEXT("Text Editor"), MB_YESNOCANCEL | MB_ICONQUESTION | MB_TOPMOST);
 			switch (status)
 			{
 			case IDYES:
